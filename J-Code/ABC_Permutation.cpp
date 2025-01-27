088e190d70a2010c8522dc57b1bc9db6b116bc08
Jaiigh <jayjaycc2004@gmail.com>
2025-01-28 03:36:48 +0700

    bruh

---
+++ J-Code/ABC_Permutation.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void pem(int N, int i, int j, int k, vector<string> &ans, int len, string now) {
+    if (len == N) {
+        ans.push_back(now);
+        return;
+    }
+
+    if (i > 0) {
+        pem(N, i - 1, j, k, ans, len + 1, now + "A");
+    }
+
+    if (j > 0) {
+        pem(N, i, j - 1, k, ans, len + 1, now + "B");
+    }
+
+    if (k > 0) {
+        pem(N, i, j, k - 1, ans, len + 1, now + "C");
+    }
+}
+
+int main() {
+    int N, i, j, k;
+    cin >> N >> i >> j >> k;
+    vector<string> ans;
+    pem(N, i, j, k, ans, 0, "");
+    cout << ans.size() << endl;
+    for (auto s : ans) {
+        cout << s << endl;
+    }
+}
